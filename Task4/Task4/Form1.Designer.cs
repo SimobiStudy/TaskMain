f26bed50b5035b925cd7f52177572984c708efe9
SimobiSirOP <simobisirop@gmail.com>
2026-03-18 10:41:33 +0300

    Adding task 4 complete

--- Task4/Task4/Form1.Designer.cs
+++ Task4/Task4/Form1.Designer.cs
@@ -29,21 +29,17 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-        System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-        System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+        System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+        System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+        System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
         tabControl1 = new TabControl();
         tabPage1 = new TabPage();
         button1 = new Button();
         label7 = new Label();
-        numericUDDown2 = new NumericUpDown();
-        label4 = new Label();
         numericUDUp2 = new NumericUpDown();
         label5 = new Label();
         label6 = new Label();
         comboBoxTeam2 = new ComboBox();
-        numericUDDown1 = new NumericUpDown();
-        label3 = new Label();
         numericUDUp1 = new NumericUpDown();
         label2 = new Label();
         label1 = new Label();
@@ -56,13 +52,14 @@ partial class Form1
         Score = new DataGridViewTextBoxColumn();
         GotGoals = new DataGridViewTextBoxColumn();
         WrongGoals = new DataGridViewTextBoxColumn();
+        Winner = new DataGridViewTextBoxColumn();
         tabPage3 = new TabPage();
         chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+        openFileDialog1 = new OpenFileDialog();
+        saveFileDialog1 = new SaveFileDialog();
         tabControl1.SuspendLayout();
         tabPage1.SuspendLayout();
-        ((System.ComponentModel.ISupportInitialize)numericUDDown2).BeginInit();
         ((System.ComponentModel.ISupportInitialize)numericUDUp2).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)numericUDDown1).BeginInit();
         ((System.ComponentModel.ISupportInitialize)numericUDUp1).BeginInit();
         tabPage2.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)dataGridMatch).BeginInit();
@@ -76,33 +73,30 @@ partial class Form1
         tabControl1.Controls.Add(tabPage2);
         tabControl1.Controls.Add(tabPage3);
         tabControl1.Location = new Point(1, -1);
-        tabControl1.Margin = new Padding(4, 3, 4, 3);
+        tabControl1.Margin = new Padding(6, 3, 6, 3);
         tabControl1.Name = "tabControl1";
         tabControl1.SelectedIndex = 0;
-        tabControl1.Size = new Size(674, 520);
+        tabControl1.Size = new Size(1052, 629);
         tabControl1.TabIndex = 0;
+        tabControl1.Click += tabControl1_Click;
         // 
         // tabPage1
         // 
         tabPage1.Controls.Add(button1);
         tabPage1.Controls.Add(label7);
-        tabPage1.Controls.Add(numericUDDown2);
-        tabPage1.Controls.Add(label4);
         tabPage1.Controls.Add(numericUDUp2);
         tabPage1.Controls.Add(label5);
         tabPage1.Controls.Add(label6);
         tabPage1.Controls.Add(comboBoxTeam2);
-        tabPage1.Controls.Add(numericUDDown1);
-        tabPage1.Controls.Add(label3);
         tabPage1.Controls.Add(numericUDUp1);
         tabPage1.Controls.Add(label2);
         tabPage1.Controls.Add(label1);
         tabPage1.Controls.Add(comboBoxTeam1);
         tabPage1.Location = new Point(4, 31);
-        tabPage1.Margin = new Padding(4, 3, 4, 3);
+        tabPage1.Margin = new Padding(6, 3, 6, 3);
         tabPage1.Name = "tabPage1";
-        tabPage1.Padding = new Padding(4, 3, 4, 3);
-        tabPage1.Size = new Size(872, 491);
+        tabPage1.Padding = new Padding(6, 3, 6, 3);
+        tabPage1.Size = new Size(1044, 594);
         tabPage1.TabIndex = 0;
         tabPage1.Text = "Ввод";
         tabPage1.UseVisualStyleBackColor = true;
@@ -110,112 +104,93 @@ partial class Form1
         // button1
         // 
         button1.Font = new Font("Roboto SemiBold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
-        button1.Location = new Point(35, 362);
+        button1.Location = new Point(55, 438);
+        button1.Margin = new Padding(5, 3, 5, 3);
         button1.Name = "button1";
-        button1.Size = new Size(601, 74);
+        button1.Size = new Size(939, 89);
         button1.TabIndex = 13;
         button1.Text = "Ввести результаты";
         button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
         // 
         // label7
         // 
         label7.Font = new Font("Roboto", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-        label7.Location = new Point(35, 31);
+        label7.Location = new Point(55, 37);
+        label7.Margin = new Padding(5, 0, 5, 0);
         label7.Name = "label7";
-        label7.Size = new Size(354, 31);
+        label7.Size = new Size(552, 37);
         label7.TabIndex = 12;
         label7.Text = "Введите результаты матча";
         // 
-        // numericUDDown2
-        // 
-        numericUDDown2.Location = new Point(475, 295);
-        numericUDDown2.Name = "numericUDDown2";
-        numericUDDown2.Size = new Size(161, 29);
-        numericUDDown2.TabIndex = 11;
-        // 
-        // label4
-        // 
-        label4.Location = new Point(475, 248);
-        label4.Name = "label4";
-        label4.Size = new Size(161, 44);
-        label4.TabIndex = 10;
-        label4.Text = "Пропущенных голов";
-        // 
         // numericUDUp2
         // 
-        numericUDUp2.Location = new Point(475, 201);
+        numericUDUp2.Location = new Point(742, 243);
+        numericUDUp2.Margin = new Padding(5, 3, 5, 3);
         numericUDUp2.Name = "numericUDUp2";
-        numericUDUp2.Size = new Size(161, 29);
+        numericUDUp2.Size = new Size(251, 29);
         numericUDUp2.TabIndex = 9;
         // 
         // label5
         // 
-        label5.Location = new Point(475, 173);
+        label5.Location = new Point(742, 209);
+        label5.Margin = new Padding(5, 0, 5, 0);
         label5.Name = "label5";
-        label5.Size = new Size(161, 29);
+        label5.Size = new Size(251, 35);
         label5.TabIndex = 8;
         label5.Text = "Забитых голов";
         // 
         // label6
         // 
-        label6.Location = new Point(549, 100);
+        label6.Location = new Point(858, 121);
+        label6.Margin = new Padding(5, 0, 5, 0);
         label6.Name = "label6";
-        label6.Size = new Size(87, 28);
+        label6.Size = new Size(136, 34);
         label6.TabIndex = 7;
         label6.Text = "Команда";
         // 
         // comboBoxTeam2
         // 
         comboBoxTeam2.FormattingEnabled = true;
-        comboBoxTeam2.Location = new Point(475, 131);
+        comboBoxTeam2.Location = new Point(742, 158);
+        comboBoxTeam2.Margin = new Padding(5, 3, 5, 3);
         comboBoxTeam2.Name = "comboBoxTeam2";
-        comboBoxTeam2.Size = new Size(161, 30);
+        comboBoxTeam2.Size = new Size(249, 30);
         comboBoxTeam2.TabIndex = 6;
-        // 
-        // numericUDDown1
-        // 
-        numericUDDown1.Location = new Point(35, 295);
-        numericUDDown1.Name = "numericUDDown1";
-        numericUDDown1.Size = new Size(161, 29);
-        numericUDDown1.TabIndex = 5;
-        // 
-        // label3
-        // 
-        label3.Location = new Point(35, 248);
-        label3.Name = "label3";
-        label3.Size = new Size(161, 44);
-        label3.TabIndex = 4;
-        label3.Text = "Пропущенных голов";
         // 
         // numericUDUp1
         // 
-        numericUDUp1.Location = new Point(35, 201);
+        numericUDUp1.Location = new Point(55, 243);
+        numericUDUp1.Margin = new Padding(5, 3, 5, 3);
         numericUDUp1.Name = "numericUDUp1";
-        numericUDUp1.Size = new Size(161, 29);
+        numericUDUp1.Size = new Size(251, 29);
         numericUDUp1.TabIndex = 3;
         // 
         // label2
         // 
-        label2.Location = new Point(35, 173);
+        label2.Location = new Point(55, 209);
+        label2.Margin = new Padding(5, 0, 5, 0);
         label2.Name = "label2";
-        label2.Size = new Size(161, 29);
+        label2.Size = new Size(251, 35);
         label2.TabIndex = 2;
         label2.Text = "Забитых голов";
         // 
         // label1
         // 
-        label1.Location = new Point(35, 100);
+        label1.Location = new Point(55, 121);
+        label1.Margin = new Padding(5, 0, 5, 0);
         label1.Name = "label1";
-        label1.Size = new Size(87, 28);
+        label1.Size = new Size(136, 34);
         label1.TabIndex = 1;
         label1.Text = "Команда";
         // 
         // comboBoxTeam1
         // 
         comboBoxTeam1.FormattingEnabled = true;
-        comboBoxTeam1.Location = new Point(35, 131);
+        comboBoxTeam1.Location = new Point(55, 158);
+        comboBoxTeam1.Margin = new Padding(5, 3, 5, 3);
         comboBoxTeam1.Name = "comboBoxTeam1";
-        comboBoxTeam1.Size = new Size(161, 30);
+        comboBoxTeam1.Size = new Size(249, 30);
         comboBoxTeam1.TabIndex = 0;
         // 
         // tabPage2
@@ -223,11 +198,11 @@ partial class Form1
         tabPage2.Controls.Add(button3);
         tabPage2.Controls.Add(button2);
         tabPage2.Controls.Add(dataGridMatch);
-        tabPage2.Location = new Point(4, 29);
-        tabPage2.Margin = new Padding(4, 3, 4, 3);
+        tabPage2.Location = new Point(4, 31);
+        tabPage2.Margin = new Padding(6, 3, 6, 3);
         tabPage2.Name = "tabPage2";
-        tabPage2.Padding = new Padding(4, 3, 4, 3);
-        tabPage2.Size = new Size(872, 493);
+        tabPage2.Padding = new Padding(6, 3, 6, 3);
+        tabPage2.Size = new Size(1044, 594);
         tabPage2.TabIndex = 1;
         tabPage2.Text = "Таблица";
         tabPage2.UseVisualStyleBackColor = true;
@@ -236,23 +211,27 @@ partial class Form1
         // 
         button3.Font = new Font("Roboto Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
         button3.ForeColor = Color.DarkBlue;
-        button3.Location = new Point(74, 390);
+        button3.Location = new Point(115, 472);
+        button3.Margin = new Padding(5, 3, 5, 3);
         button3.Name = "button3";
-        button3.Size = new Size(165, 67);
+        button3.Size = new Size(258, 81);
         button3.TabIndex = 2;
         button3.Text = "Сохранить в файл";
         button3.UseVisualStyleBackColor = true;
+        button3.Click += button3_Click;
         // 
         // button2
         // 
         button2.Font = new Font("Roboto Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
         button2.ForeColor = Color.Crimson;
-        button2.Location = new Point(400, 390);
+        button2.Location = new Point(625, 472);
+        button2.Margin = new Padding(5, 3, 5, 3);
         button2.Name = "button2";
-        button2.Size = new Size(165, 67);
+        button2.Size = new Size(258, 81);
         button2.TabIndex = 1;
         button2.Text = "Загрузить из файла";
         button2.UseVisualStyleBackColor = true;
+        button2.Click += button2_Click;
         // 
         // dataGridMatch
         // 
@@ -261,15 +240,17 @@ partial class Form1
         dataGridMatch.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
         dataGridMatch.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
         dataGridMatch.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridMatch.Columns.AddRange(new DataGridViewColumn[] { Команды, Score, GotGoals, WrongGoals });
-        dataGridMatch.Location = new Point(7, 38);
+        dataGridMatch.Columns.AddRange(new DataGridViewColumn[] { Команды, Score, GotGoals, WrongGoals, Winner });
+        dataGridMatch.Location = new Point(11, 46);
+        dataGridMatch.Margin = new Padding(5, 3, 5, 3);
         dataGridMatch.Name = "dataGridMatch";
         dataGridMatch.ReadOnly = true;
         dataGridMatch.RowHeadersVisible = false;
         dataGridMatch.RowHeadersWidth = 51;
-        dataGridMatch.Size = new Size(631, 279);
+        dataGridMatch.Size = new Size(986, 338);
         dataGridMatch.TabIndex = 0;
         dataGridMatch.Text = "dataGridView1";
+        dataGridMatch.CellContentClick += dataGridMatch_CellContentClick;
         // 
         // Команды
         // 
@@ -303,40 +284,58 @@ partial class Form1
         WrongGoals.ReadOnly = true;
         WrongGoals.Width = 240;
         // 
+        // Winner
+        // 
+        Winner.HeaderText = "Победитель";
+        Winner.MinimumWidth = 6;
+        Winner.Name = "Winner";
+        Winner.ReadOnly = true;
+        Winner.Width = 141;
+        // 
         // tabPage3
         // 
         tabPage3.Controls.Add(chart1);
-        tabPage3.Location = new Point(4, 31);
-        tabPage3.Margin = new Padding(4, 3, 4, 3);
+        tabPage3.Location = new Point(4, 29);
+        tabPage3.Margin = new Padding(6, 3, 6, 3);
         tabPage3.Name = "tabPage3";
-        tabPage3.Size = new Size(666, 485);
+        tabPage3.Size = new Size(1044, 596);
         tabPage3.TabIndex = 2;
         tabPage3.Text = "Диаграмма";
         tabPage3.UseVisualStyleBackColor = true;
         // 
         // chart1
         // 
-        chartArea2.Area3DStyle.Enable3D = true;
-        chartArea2.Name = "ChartArea1";
-        chart1.ChartAreas.Add(chartArea2);
-        legend2.Name = "Legend1";
-        chart1.Legends.Add(legend2);
-        chart1.Location = new Point(9, 7);
+        chartArea1.Name = "ChartArea1";
+        chart1.ChartAreas.Add(chartArea1);
+        legend1.Name = "Legend1";
+        chart1.Legends.Add(legend1);
+        chart1.Location = new Point(115, 147);
         chart1.Name = "chart1";
-        series2.ChartArea = "ChartArea1";
-        series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-        series2.Legend = "Legend1";
-        series2.Name = "Series1";
-        chart1.Series.Add(series2);
-        chart1.Size = new Size(649, 444);
+        series1.ChartArea = "ChartArea1";
+        series1.Legend = "Legend1";
+        series1.Name = "Series1";
+        chart1.Series.Add(series1);
+        chart1.Size = new Size(607, 377);
         chart1.TabIndex = 0;
         chart1.Text = "chart1";
+        // 
+        // openFileDialog1
+        // 
+        openFileDialog1.FileName = "openFileDialog1";
+        openFileDialog1.Filter = "Match файл|*t4ma";
+        openFileDialog1.FileOk += openFileDialog1_FileOk;
+        // 
+        // saveFileDialog1
+        // 
+        saveFileDialog1.DefaultExt = "t4ma";
+        saveFileDialog1.FileName = "matchL";
+        saveFileDialog1.Filter = "Match файл|*t4ma";
         // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(10F, 22F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(675, 516);
+        ClientSize = new Size(1055, 625);
         Controls.Add(tabControl1);
         Font = new Font("Roboto", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
         Margin = new Padding(4, 3, 4, 3);
@@ -344,9 +343,7 @@ partial class Form1
         Text = "Футбольные результаты";
         tabControl1.ResumeLayout(false);
         tabPage1.ResumeLayout(false);
-        ((System.ComponentModel.ISupportInitialize)numericUDDown2).EndInit();
         ((System.ComponentModel.ISupportInitialize)numericUDUp2).EndInit();
-        ((System.ComponentModel.ISupportInitialize)numericUDDown1).EndInit();
         ((System.ComponentModel.ISupportInitialize)numericUDUp1).EndInit();
         tabPage2.ResumeLayout(false);
         ((System.ComponentModel.ISupportInitialize)dataGridMatch).EndInit();
@@ -359,26 +356,15 @@ partial class Form1
 
     private System.Windows.Forms.Button button2;
 
-    private System.Windows.Forms.DataGridViewTextBoxColumn Score;
-
-    private System.Windows.Forms.DataGridViewTextBoxColumn Команды;
-    private System.Windows.Forms.DataGridViewTextBoxColumn GotGoals;
-    private System.Windows.Forms.DataGridViewTextBoxColumn WrongGoals;
-
     private System.Windows.Forms.DataGridView dataGridMatch;
 
     private System.Windows.Forms.Button button1;
 
-    private System.Windows.Forms.NumericUpDown numericUDDown2;
-    private System.Windows.Forms.Label label4;
     private System.Windows.Forms.NumericUpDown numericUDUp2;
     private System.Windows.Forms.Label label5;
     private System.Windows.Forms.Label label6;
     private System.Windows.Forms.ComboBox comboBoxTeam2;
     private System.Windows.Forms.Label label7;
-
-    private System.Windows.Forms.NumericUpDown numericUDDown1;
-    private System.Windows.Forms.Label label3;
 
     private System.Windows.Forms.Label label2;
     private System.Windows.Forms.NumericUpDown numericUDUp1;
@@ -394,6 +380,12 @@ partial class Form1
     private System.Windows.Forms.TabPage tabPage2;
 
     #endregion
-
+    private DataGridViewTextBoxColumn Команды;
+    private DataGridViewTextBoxColumn Score;
+    private DataGridViewTextBoxColumn GotGoals;
+    private DataGridViewTextBoxColumn WrongGoals;
+    private DataGridViewTextBoxColumn Winner;
     private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    private OpenFileDialog openFileDialog1;
+    private SaveFileDialog saveFileDialog1;
 }
--- Task4/Task4/Form1.cs
+++ Task4/Task4/Form1.cs
@@ -1,9 +1,209 @@
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace Task4;
 
 public partial class Form1 : Form
 {
+    public List<Match> matches = new();
+
     public Form1()
     {
         InitializeComponent();
+        saveFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        var team1Name = comboBoxTeam1.Text;
+        var team2Name = comboBoxTeam2.Text;
+        matches.Add(new Match
+        {
+            Team1 = new Team
+            { Name = team1Name, ShotGoals = (int)numericUDUp1.Value, GotGoals = (int)numericUDUp2.Value },
+            Team2 = new Team
+            { Name = team2Name, ShotGoals = (int)numericUDUp2.Value, GotGoals = (int)numericUDUp1.Value }
+        });
+    }
+
+    private void dataGridMatch_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+    }
+
+    private void FillDataGrid()
+    {
+        dataGridMatch.Rows.Clear();
+        foreach (var match in matches)
+        {
+            var dataGridRow = new DataGridViewRow();
+            dataGridRow.Cells.AddRange(
+                new DataGridViewTextBoxCell { Value = $"{match.Team1.Name} - {match.Team2.Name}" },
+                new DataGridViewTextBoxCell { Value = $"{match.Team1.GetScore()} - {match.Team2.GetScore()}" },
+                new DataGridViewTextBoxCell { Value = $"{match.Team1.ShotGoals} - {match.Team2.ShotGoals}" },
+                new DataGridViewTextBoxCell { Value = $"{match.Team1.GotGoals} - {match.Team2.GotGoals}" },
+                new DataGridViewTextBoxCell { Value = $"{match.GetWinner().Name}" }
+            );
+            dataGridMatch.Rows.Add(dataGridRow);
+        }
+    }
+
+    private void FillDiagram()
+    {
+        chart1.Series.Clear();
+        // Форматировать диаграмму.
+        chart1.BackColor = Color.Gray;
+        chart1.BackSecondaryColor = Color.WhiteSmoke;
+        chart1.BackGradientStyle = GradientStyle.DiagonalRight;
+
+        chart1.BorderlineDashStyle = ChartDashStyle.Solid;
+        chart1.BorderlineColor = Color.Gray;
+        chart1.BorderSkin.SkinStyle = BorderSkinStyle.None;
+
+        // Форматировать область диаграммы
+        chart1.ChartAreas[0].BackColor = Color.White;
+        // chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
+
+        // Добавить и форматировать заголовок.
+        chart1.Titles.Clear();
+        chart1.Titles.Add("Результаты матчей в турнире");
+        chart1.Titles[0].Font = new Font("Roboto", 20);
+
+        var series = new Series("Очки команды");
+        series.ChartType = SeriesChartType.Column;
+
+        var teams = new Dictionary<string, int>();
+        foreach (var match in matches)
+        {
+            if (!teams.ContainsKey(match.Team1.Name))
+                teams.Add(match.Team1.Name, match.Team1.GetScore());
+            else
+                teams[match.Team1.Name] += match.Team1.GetScore();
+
+            if (!teams.ContainsKey(match.Team2.Name))
+                teams.Add(match.Team2.Name, match.Team2.GetScore());
+            else
+                teams[match.Team2.Name] += match.Team2.GetScore();
+        }
+
+        var counter = 0;
+        foreach (var team in teams)
+        {
+            var index1 = series.Points.AddXY(counter, team.Value);
+            series.Points[index1].AxisLabel = team.Key;
+            counter++;
+        }
+
+        chart1.Series.Add(series);
+    }
+
+    private void tabControl1_Click(object sender, EventArgs e)
+    {
+        if (tabControl1.SelectedIndex == 1)
+            FillDataGrid();
+        if (tabControl1.SelectedIndex == 2)
+            FillDiagram();
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        var dialogResult = saveFileDialog1.ShowDialog();
+        if (dialogResult != DialogResult.OK) return;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (var match in matches)
+        {
+            stringBuilder.AppendLine(match.ToString());
+        }
+        File.WriteAllText(saveFileDialog1.FileName, stringBuilder.ToString());
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        var dialogResult = openFileDialog1.ShowDialog();
+        if (dialogResult != DialogResult.OK) return;
+
+        matches.Clear();
+        foreach (var line in File.ReadAllLines(openFileDialog1.FileName))
+        {
+            try
+            {
+                matches.Add(LoadFromString(line));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке строки: {line}\n{ex.Message}");
+            }
+        }
+        FillDataGrid();
+    }
+
+    private readonly string regexPattern = @"^(.+)-(.+);(-?\d+)-(-?\d+);(-?\d+)-(-?\d+)";
+    private Match LoadFromString(string strToLoad)
+    {
+        var match = Regex.Match(strToLoad.Trim(), regexPattern);
+        if (!match.Success)
+        {
+            throw new Exception("Неверный формат строки");
+        }
+
+
+        Match matchToAdd = new Match
+        {
+            Team1 = new Team
+            {
+                Name = match.Groups[1].Value,
+                ShotGoals = int.Parse(match.Groups[3].Value),
+                GotGoals = int.Parse(match.Groups[5].Value)
+            },
+            Team2 = new Team
+            {
+                Name = match.Groups[2].Value,
+                ShotGoals = int.Parse(match.Groups[4].Value),
+                GotGoals = int.Parse(match.Groups[6].Value)
+            }
+        };
+        return matchToAdd;
+    }
+
+    private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+
+    }
+
+    
+}
+
+public class Match
+{
+    public Team Team1 { get; set; }
+    public Team Team2 { get; set; }
+
+    public Team GetWinner()
+    {
+        var score1 = Team1.GetScore();
+        var score2 = Team2.GetScore();
+        if (score1 > score2)
+            return Team1;
+        if (score2 > score1)
+            return Team2;
+        return null;
+    }
+
+    public override string ToString()
+    {
+        return $"{Team1.Name}-{Team2.Name};{Team1.ShotGoals}-{Team2.ShotGoals};{Team1.GotGoals}-{Team2.GotGoals}";
+    }
+}
+
+public class Team
+{
+    public string Name { get; set; }
+    public int ShotGoals { get; set; }
+    public int GotGoals { get; set; }
+
+    public int GetScore()
+    {
+        return ShotGoals - GotGoals;
     }
 }
