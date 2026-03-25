aca320a2096489d869631979158aa598cacbf33b
SimobiSirOP <simobisirop@gmail.com>
2026-03-25 09:30:44 +0300

    Basic XML file representation for task 6

--- Task6/QuestionsXML/QuestionXML.cs
+++ Task6/QuestionsXML/QuestionXML.cs
@@ -5,12 +5,22 @@ namespace QuestionsXML;
 
 public static class ThemeXmlH
 {
+    /// <summary>
+    /// Используйте этот метод для получения класса <see cref="Theme"/> из файла
+    /// </summary>
+    /// <param name="fileName">Путь к файлу</param>
+    /// <returns>Класс <see cref="Theme"/></returns>
     public static Theme GetTheme(string fileName)
     {
         var serializer = new XmlSerializer(typeof(Theme));
         return (Theme)serializer.Deserialize(new StringReader(File.ReadAllText(fileName)));
     }
 
+    /// <summary>
+    /// Используйте этот метод для сериализации <see cref="Theme"/> в файл
+    /// </summary>
+    /// <param name="theme">Класс <see cref="Theme"/></param>
+    /// <param name="fileName">Путь к файлу для сохранения</param>
     public static void SaveTheme(this Theme theme, string fileName)
     {
         var serializer = new XmlSerializer(typeof(Theme));
