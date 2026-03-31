57919b2f036030243eb21001bff8d8684d1c149b
SimobiSirOP <simobisirop@gmail.com>
2026-03-31 10:36:16 +0300

    full task 7 done

--- Task7/Task7/Main/Figures/SquareWithHole.cs
+++ Task7/Task7/Main/Figures/SquareWithHole.cs
@@ -6,6 +6,7 @@ namespace Task7.Main.Figures
 {
     internal class SquareWithHole : Square
     {
+        public SquareWithHole() {}
         public SquareWithHole(Point location, Size size) : base(location, size)
         { }
 
@@ -20,6 +21,18 @@ namespace Task7.Main.Figures
             Rectangle holeRect = new Rectangle(new Point(holeX, holeY), new Size(_boundingBox.Width, _boundingBox.Height));
             using Brush holeBrush = new SolidBrush(Color.White); // Цвет дырки (можно изменить)
             graphics.FillEllipse(holeBrush, holeRect);
+        }
+
+        public override object Clone()
+        {
+            var square = new SquareWithHole()
+            {
+                FigureStroke = (Stroke)this.FigureStroke.Clone(),
+                FigureColor = this.FigureColor,
+                Location = this.Location,
+                Size = this.Size
+            };
+            return square;
         }
     }
 } 
