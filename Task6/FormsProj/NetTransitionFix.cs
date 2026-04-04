030679314673ba3c91679a0d18d0bac1e0a09a15
SimobiSirOP <simobisirop@gmail.com>
2026-04-04 14:14:22 +0300

    Task 6 transition

---
+++ Task6/FormsProj/NetTransitionFix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsProj
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Replicates the .NET 10 Shuffle functionality for .NET 9.
+        /// </summary>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random? random = null)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            // Use Random.Shared if no specific random instance is provided
+            var rng = random ?? Random.Shared;
+
+            // We must buffer the collection to shuffle it
+            var buffer = source.ToArray();
+
+            for (int i = buffer.Length - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                // Modern C# tuple swap
+                (buffer[i], buffer[j]) = (buffer[j], buffer[i]);
+            }
+
+            foreach (var item in buffer)
+            {
+                yield return item;
+            }
+        }
+    }
+}
